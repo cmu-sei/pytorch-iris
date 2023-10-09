cfecc16b7aa303d2b3b1bb30fad6a4c87d6704c3
John G Wohlbier <jgwohlbier@mac-chiliapple.ad.sei.cmu.edu>
2023-10-09 11:56:00 -0400

    Modify training script to write inputs / outputs and to write to a folder

--- train.py
+++ train.py
@@ -34,30 +34,66 @@ import torch
 from torch.ao.quantization import get_default_qconfig_mapping, QConfigMapping
 from torch.ao.quantization.quantize_fx import convert_fx, fuse_fx, prepare_fx
 from torch.autograd import Variable
+import os
+
+def write_matrix_to_file(filename, variable):
+    file_delimiter = " "
+    rows, columns = variable.shape
+    file_str = [""]*rows
+    for row in range(0, rows):
+        if row > 0:
+            file_str[row] += "\n"
+        for column in range(0, columns):
+            if column > 0:
+                file_str[row] += file_delimiter
+            file_str[row] += str(variable[row][column].item())
+    with open(filename, "w") as w_file:
+        w_file.writelines(file_str)
 
 def main():
     print("PyTorch Iris train")
     print(torch.backends.quantized.supported_engines)
 
-    verbose = True
+    verbose = False
     X,y,names,feature_names = m.load_data()
     if verbose:
         m.vis_data(X,y,names,feature_names)
     X_train,X_test,y_train,y_test = m.split_data(X,y)
+    
+    # make an output directory
+    data_dir = "data"
+    if not os.path.exists(data_dir):
+        os.mkdir(data_dir)
 
     # fp32
     quantize = False
     model_fp32 = m.train_model(X_train,X_test,y_train,y_test,quantize,verbose)
     if verbose:
         m.plot_roc_curve(model_fp32,X_test,y_test,"fp32")
-    m.save_model(model_fp32,"model_fp32.pt",quantize)
+    m.save_model(model_fp32, os.path.join(data_dir, "model_fp32.pt"), quantize)
 
     # quantized to int8 with quantized aware training using graph_fx
-    quantize = True
-    model_int8 = m.train_model(X_train,X_test,y_train,y_test,quantize,verbose)
-    if verbose:
-        m.plot_roc_curve(model_int8,X_test,y_test,"int8")
-    m.save_model(model_int8,"./model_int8.pt",quantize)
+    isBuildingQuantizedModel = False
+    if isBuildingQuantizedModel:
+        quantize = True
+        model_int8 = m.train_model(X_train,X_test,y_train,y_test,quantize,verbose)
+        if verbose:
+            m.plot_roc_curve(model_int8,X_test,y_test,"int8")
+        m.save_model(model_int8, os.path.join(data_dir, "model_int8.pt"), quantize)
+
+    # input data and output predictions
+    X_test  = Variable(torch.from_numpy(X_test)).float()
+    y_pred = model_fp32(X_test)
+
+    # sanity check
+    y_test_pred = torch.argmax(y_pred, dim=1)
+    y_test = Variable(torch.from_numpy(y_test)).long()
+    model_accuracy = sum(y_test_pred == y_test) / len(y_test)
+    print("accuracy check: " + str(model_accuracy.item()))
+
+    # write input data and output predictions to a file
+    write_matrix_to_file(os.path.join(data_dir, "Input.dat"), X_test)
+    write_matrix_to_file(os.path.join(data_dir, "Output.dat"), y_pred)
 
 if __name__ == "__main__":
     main()
